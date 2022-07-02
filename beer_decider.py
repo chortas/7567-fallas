5893fb6cf015ac975ff867808faf4a6531dc2e66
Oli <olifer97@gmail.com>
2022-07-02 17:20:34 -0300

    add color, malts, hops, yeast

--- beer_decider.py
+++ beer_decider.py
@@ -17,7 +17,7 @@ class BeerDecider(KnowledgeEngine):
                        lambda ag: 4.1 <= ag <= 5.1),
                    bitterness=MATCH.ibu & P(lambda ibu: 15 <= ibu <= 25)))
     def pale_ale(self):
-        beer = Beer(name="Pale Ale")
+        beer = Beer(name="Pale Ale", color="Rubia", malts=["pale", "caramel", "munich"], hops=["cascade", "centennial"], yeast=["ale"])
         self.declare(beer)
 
     @Rule(BeerInfo(initial_gravity="Regular",
@@ -27,7 +27,7 @@ class BeerDecider(KnowledgeEngine):
                        lambda ag: 6.3 <= ag <= 7.5),
                    bitterness=MATCH.ibu & P(lambda ibu: 50 <= ibu <= 70)))
     def ipa(self):
-        beer = Beer(name="IPA")
+        beer = Beer(name="IPA", color="Roja", malts=["american two-row"], hops=["centennial", "simcoe", "amarillo"], yeast=["ale"])
         self.declare(beer)
 
     @Rule(BeerInfo(initial_gravity="Poco",
@@ -37,7 +37,7 @@ class BeerDecider(KnowledgeEngine):
                        lambda ag: 7 <= ag <= 12),
                    bitterness=MATCH.ibu & P(lambda ibu: 35 <= ibu <= 50)))
     def imperial_porter(self):
-        beer = Beer(name="Imperial Porter")
+        beer = Beer(name="Imperial Porter", color="Negra", malts=["varies"], hops=["varies"], yeast=["ale"])
         self.declare(beer)
 
     @ Rule(BeerInfo(initial_gravity="Muy poco",
@@ -47,7 +47,7 @@ class BeerDecider(KnowledgeEngine):
                         lambda ag: 5.7 <= ag <= 8.7),
                     bitterness=MATCH.ibu & P(lambda ibu: 35 <= ibu <= 60)))
     def stout(self):
-        beer = Beer(name="Stout")
+        beer = Beer(name="Stout", color="Negra", malts=["pale", "black roasted barley", "special B", "caramunich", "chocolate", "pale chocolate"], hops=["horizon", "Kent Goldings"], yeast=["ale"])
         self.declare(beer)
 
     @ Rule(BeerInfo(initial_gravity="Poco",
@@ -57,7 +57,7 @@ class BeerDecider(KnowledgeEngine):
                         lambda ag: 4.4 <= ag <= 6.1),
                     bitterness=MATCH.ibu & P(lambda ibu: 25 <= ibu <= 45)))
     def amber_ale(self):
-        beer = Beer(name="Amber Ale")
+        beer = Beer(name="Amber Ale", color="Roja", malts=["english Pale Ale", "american two-row", "crystal", "victory"], hops=["horizon", "cascade", "centennial"], yeast=["ale"])
         self.declare(beer)
 
     @ Rule(BeerInfo(initial_gravity="Todo",
@@ -67,7 +67,7 @@ class BeerDecider(KnowledgeEngine):
                         lambda ag: 8.5 <= ag <= 12.2),
                     bitterness=MATCH.ibu & P(lambda ibu: 60 <= ibu <= 100)))
     def american_barley_wine(self):
-        beer = Beer(name="American Barley Wine")
+        beer = Beer(name="American Barley Wine", color="Roja", malts=["pale", "crystal", "pale chocolate", "special B"], hops=["magnum", "chinook", "centennial", "amarillo"], yeast=["ale"])
         self.declare(beer)
 
     @ Rule(BeerInfo(initial_gravity="Muy poco",
@@ -77,10 +77,10 @@ class BeerDecider(KnowledgeEngine):
                         lambda ag: 4.1 <= ag <= 5.1),
                     bitterness=MATCH.ibu & P(lambda ibu: 15 <= ibu <= 25)))
     def blonde_ale(self):
-        beer = Beer(name="Blonde Ale")
+        beer = Beer(name="Blonde Ale", color="Rubia", malts=["american two-row", "crystal"], hops=["williamette"], yeast=["lagger", "ale"])
         self.declare(beer)
 
-    @ Rule(Beer(name=MATCH.beer_name))
-    def chosen_beer(self, beer_name):
-        self.beers.append(Beer(name=beer_name))
+    @ Rule(Beer(name=MATCH.beer_name, color=MATCH.beer_color, malts=MATCH.beer_malts, hops=MATCH.beer_hops, yeast=MATCH.beer_yeast))
+    def chosen_beer(self, beer_name, beer_color, beer_malts, beer_hops, beer_yeast):
+        self.beers.append(Beer(name=beer_name, color=beer_color, malts=beer_malts, hops=beer_hops, yeast=beer_yeast))
         print(f"Chosen beer: {beer_name}")
